f48e61db06926045ab03e0211ff11a451422ee5d
Unofficial_Storm <90640046+UnofficialStorm@users.noreply.github.com>
2021-09-24 18:52:06 +0100

    Add files via upload

---
+++ tomsa.py
@@ -0,0 +1,18 @@
+
+num = input("Enter a 3 digit number")
+length = len(num) 
+
+if length != 3:
+    num = input("Enter a 3 digit number")
+else:
+    print("Processing...")
+
+count = 0
+word_p = 0
+lst = ["hundreds", "tenths", "units"]
+
+while count != length:
+    print(num[count], lst[word_p])
+    word_p = word_p + 1
+    count = count + 1
+
